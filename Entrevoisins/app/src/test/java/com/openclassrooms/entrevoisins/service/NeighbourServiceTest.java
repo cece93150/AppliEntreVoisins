fa8bc28b6f00bafa4dccfedd535ee6acfd3499b9
cece93150 <cece93150@hotmail.fr>
2022-04-14 12:26:40 +0200

    update unit test

--- Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -1,6 +1,5 @@
 package com.openclassrooms.entrevoisins.service;
 
-import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
@@ -48,29 +47,29 @@ public class NeighbourServiceTest {
     @Test
     public void getFavoriteListWithSuccess() {
         service.getFavNeighbours().clear();
-        service.getNeighbours().get(0).setFavoriteNeighbour(true);
-        service.getNeighbours().get(3).setFavoriteNeighbour(true);
+        service.getNeighbours().get(0).setFavorite(true);
+        service.getNeighbours().get(3).setFavorite(true);
         List<Neighbour> favoriteNeighbourList = service.getFavNeighbours();
         assertFalse(favoriteNeighbourList.isEmpty());
-        assertEquals(2, favoriteNeighbourList.size());
+        assertTrue(service.getFavNeighbours().get(0).getFavorite());
     }
 
     @Test
     public void deleteFavoriteNeighbourWithSuccess() {
         service.getFavNeighbours().clear();
-        service.getNeighbours().get(0).setFavoriteNeighbour(true);
-        service.getNeighbours().get(3).setFavoriteNeighbour(true);
+        service.getNeighbours().get(0).setFavorite(true);
+        service.getNeighbours().get(3).setFavorite(true);
         assertEquals(2, service.getFavNeighbours().size());
         Neighbour neighbour = service.getNeighbours().get(0);
         service.toggleNeighbourFavorite(neighbour);
         assertEquals(1, service.getFavNeighbours().size());
-        assertFalse(service.getNeighbours().get(0).isFavorite());
+        assertFalse(service.getNeighbours().get(0).getFavorite());
     }
 
     @Test
     public void addFavorite() {
         Neighbour neighbour0 = service.getNeighbours().get(0);
-        neighbour0.setFavoriteNeighbour(false);
+        neighbour0.setFavorite(false);
         service.toggleNeighbourFavorite(neighbour0);
         assertTrue(service.getFavNeighbours().contains(neighbour0));
     }
@@ -78,7 +77,7 @@ public class NeighbourServiceTest {
     @Test
     public void deleteFavorite() {
         Neighbour favorite = service.getNeighbours().get(0);
-        favorite.setFavoriteNeighbour(true);
+        favorite.setFavorite(true);
         service.toggleNeighbourFavorite(favorite);
         assertFalse(service.getFavNeighbours().contains(favorite));
     }
